2bef30ae1ce1d84fdeef34025178f860d08ca31e
Adrian Garcia Lomas <glomadrian@gmail.com>
2017-12-06 11:02:22 +0100

    Add base presenter with coroutines auto job cancellation

---
+++ core-presentation/src/main/java/com/github/glomadrian/mytaxi/corepresentation/presentation/Presenter.kt
@@ -0,0 +1,33 @@
+package com.github.glomadrian.mytaxi.corepresentation.presentation
+
+import com.github.glomadrian.mytaxi.core.BACKGROUND
+import com.github.glomadrian.mytaxi.core.MAIN
+import kotlinx.coroutines.experimental.*
+import kotlin.coroutines.experimental.CoroutineContext
+
+abstract class Presenter<T> {
+    protected var view: T? = null
+    private val presenterJob = Job()
+
+    fun onAttach(view: T) {
+        this.view = view
+    }
+
+    fun onDetach() {
+        view = null
+        presenterJob.cancel()
+    }
+
+    protected fun <T> bg(
+            context: CoroutineContext = BACKGROUND,
+            block: suspend CoroutineScope.() -> T)
+            : Deferred<T> = async(presenterJob + context) {
+        block()
+    }
+
+    protected fun main(context: CoroutineContext = MAIN,
+                       block: suspend CoroutineScope.() -> T): Job =
+            launch(presenterJob + context) {
+                block
+            }
+}
