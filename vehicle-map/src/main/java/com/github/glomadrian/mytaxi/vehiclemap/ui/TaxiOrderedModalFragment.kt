16bc3f76e5619cd1a48967ec22182d4131641c3e
Adrian Garcia Lomas <glomadrian@gmail.com>
2017-12-07 16:27:30 +0100

    Add background with rounded style

--- vehicle-map/src/main/java/com/github/glomadrian/mytaxi/vehiclemap/ui/TaxiOrderedModalFragment.kt
+++ vehicle-map/src/main/java/com/github/glomadrian/mytaxi/vehiclemap/ui/TaxiOrderedModalFragment.kt
@@ -1,10 +1,13 @@
 package com.github.glomadrian.mytaxi.vehiclemap.ui
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.view.animation.AnimationUtils
 import com.github.glomadrian.mytaxi.vehiclemap.R
 import kotlinx.android.synthetic.main.taxi_ordered.*
@@ -25,6 +28,9 @@ class TaxiOrderedModalFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        setStyle(DialogFragment.STYLE_NO_FRAME, android.R.style.Theme)
         initAnimation()
     }
 
